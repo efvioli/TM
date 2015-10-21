8b7bc15f4febb0079c000d72d2cbe323066e99e0
efvioli <efvioli@gmail.com>
2015-10-21 20:23:22 +1100

    Signed-off-by: efvioli <efvioli@gmail.com>

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpawner : MonoBehaviour {
+	public GameObject[] requiredResources;
+	public int[] stock;
+	public GameObject[] pathPoints;
+	public GameObject spawnObject;
+	public float spawnTime = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	void Spawn(){
+		bool allowSpawn = true;
+
+		//if it needs a resource
+		if(requiredResources.Length > 0)
+		{
+			//check the stock level
+			for(int i = 0; i < requiredResources.Length; i++)
+			{
+				if(stock[i] <= 0 )
+				{
+					allowSpawn = false;
+				}
+			}
+		}
+
+		if(allowSpawn)
+		{
+			//reduce the stock level
+			for(int i = 0; i <stock.Length; i++)
+			{
+				stock[i]--;
+			}
+			//spawn the resource
+			GameObject reference = Instantiate(spawnObject, transform.position, Quaternion.identity) as GameObject;
+			//set path information
+			reference.SendMessage ("SetPathPoints", pathPoints, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		for(int i = 0; i < requiredResources.Length; i++)
+		{
+			//if the colliding resource is in the requiredResource array, update the stock level
+			if(other.gameObject.tag == requiredResources[i].tag)
+			{
+				stock[i]++;
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/PathThroughObjects.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathThroughObjects : MonoBehaviour {
+	//To get it working for now
+	//need to figure out how the resources are actually moved.
+	public GameObject[] pathPoints;
+	public float speed = 1.0f;
+	public float goalSize = 0.1f;
+
+	private int currentPathIndex = 0;
+	private Vector3 movementDirection;
+	// Use this for initialization
+	void Start () {
+		movementDirection = (pathPoints[currentPathIndex].transform.position - transform.position).normalized;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//movement code
+		transform.position += movementDirection*speed*Time.deltaTime;
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		//if the object that was triggered is the current 'move to' object
+		if(other.gameObject.name == pathPoints[currentPathIndex].name)
+		{
+			//set to the same position as the 'move to' object
+			transform.position = pathPoints[currentPathIndex].transform.position;
+			currentPathIndex++;
+			//if there are no more objects in the 'move to' list
+			if(currentPathIndex >= pathPoints.Length)
+			{
+				//ADD LOGIC TO DO SOMETHING
+				Destroy(gameObject);
+			}
+			//go to next 'move to' object
+			else
+			{
+				movementDirection = (pathPoints[currentPathIndex].transform.position - transform.position).normalized;
+			}
+		}
+	}
+	
+	void SetPathPoints(GameObject[] inputPathPoints)
+	{
+		pathPoints = inputPathPoints;
+	}
+}
